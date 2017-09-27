3507f750958921b4e5f2fb82daae8f233daebbbf
pndmev <pndmev@gmail.com>
2017-09-27 19:29:41 +0300

    main

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#include "main1.cpp"
+#include "main2.cpp"
+#include "main3.cpp"
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cout << "Please, enter the number of laboratory work (1 - 3)" << endl;
+    cin >> number;
+    while (number < 1 || number > 3)
+    {
+        cout << "Incorrect number" << endl;
+        cin >> number;
+    }
+    switch (number)
+    {
+        case 1:
+        {
+            main1();
+            break;
+        }
+        case 2:
+        {
+            main2();
+            break;
+        }
+        case 3:
+        {
+            main3();
+            break;
+        }
+    }
+    return 0;
+}
