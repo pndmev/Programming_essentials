66e8090839546bdfbec5e019864d9fb1fa318067
pndmev <pndmev@gmail.com>
2017-10-11 10:44:51 +0300

    New main5.cpp

--- N_5_PonyavinD.cpp
+++ N_5_PonyavinD.cpp
@@ -2,14 +2,53 @@
 
 using namespace std;
 
-int number_of_minimal_element5 (int *a, int n, int begin)
+int* cin_array(int &n)
 {
-    int MinimalElement = a[begin]; /// minimal element in array
-    int NumberOfMinimalElement = begin; /// number of minimal element in array
-    for (int i = begin + 1; i < n; i++)
-        if (a[i] < MinimalElement)
+    cout << "Please, enter the number of elements of array" << endl;
+    cin >> n;
+    int *a = new int [n]; /// array with integers
+    cout << "Please, enter elements of array" << endl;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    return a;
+}
+
+int** cin_square_matrix(int &n)
+{
+    cout << "Please, enter the number of rows in square matrix" << endl;
+    cin >> n;
+    int** a = new int*[n]; /// array with integers
+    for(int i = 0; i < n; i++)
+        a[i] = new int[n];
+    cout << "Please, enter elements of matrix" << endl;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> a[i][j];
+    return a;
+}
+
+void cout_array(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << endl;
+}
+
+void swap5 (int &a, int &b)
+{
+    int bufer = a;
+    a = b;
+    b = bufer;
+}
+
+int number_of_minimal_element5 (int *a, int n)
+{
+    int MinimalElement = *a; /// minimal element in array
+    int NumberOfMinimalElement = 0; /// number of minimal element in array
+    for (int i = 1; i < n; i++)
+        if (*(a + i) < MinimalElement)
         {
-            MinimalElement = a[i];
+            MinimalElement = *(a + i);
             NumberOfMinimalElement = i;
         }
     return NumberOfMinimalElement;
@@ -20,7 +59,7 @@ void buble_sort5 (int *a, int n)
     for (int i = 0; i < n; i++)
         for (int j = n - 1; j > i; j--)
             if (a[j] < a[i])
-                swap(a[j], a[i]);
+                swap5(a[j], a[i]);
 }
 
 void selection_sort5 (int *a, int n)
@@ -28,17 +67,17 @@ void selection_sort5 (int *a, int n)
     int NumberOfMinimalElement; /// number of minimal element in array
     for (int i = 0; i < n; i++)
     {
-        NumberOfMinimalElement = number_of_minimal_element5(a, n, i);
+        NumberOfMinimalElement = number_of_minimal_element5(a + i, n - i) + i;
         if (a[i] != a[NumberOfMinimalElement])
-            swap(a[i], a[NumberOfMinimalElement]);
+            swap5(a[i], a[NumberOfMinimalElement]);
     }
 }
 
-int sum_line5 (int **a, int n, int NumberOfLine)
+int sum_row5 (int *a, int n)
 {
     int sum = 0;
     for (int i = 0; i < n; i++)
-        sum += a[NumberOfLine][i];
+        sum += a[i];
     return sum;
 }
 
@@ -79,6 +118,24 @@ void insert_sort5 (int *a, int n)
             a[j] = a[j - 1];
         *ptr = x;
     }
+}
+
+bool check_magic_square (int **a, int n)
+{
+    int sum = sum_row5(a[0], n); /// value of sum of first line
+    /// Processing and check (line)
+    for (int i = 1; i < n; i++)
+        if (sum_row5(a[i], n) != sum)
+        {
+            return false;
+        }
+    /// Processing and check (column)
+    for (int i = 0; i < n; i++)
+        if (sum_column5(a, n, i) != sum)
+        {
+            return false;
+        }
+    return true;
 }
 
 int* check_subset5(int *A, int nA, int *B, int nB)
@@ -150,114 +207,63 @@ int main5()
         case 1:
         {
             int n; /// number of elements
-            cout << "Please, enter the number of elements of array" << endl;
-            cin >> n;
-            int *a = new int [n]; /// array with integers
-            cout << "Please, enter elements of array" << endl;
-            for (int i = 0; i < n; i++)
-                cin >> a[i];
-
-            buble_sort5(a, n);
-            for (int i = 0; i < n; i++)
-                cout << a[i] << " ";
+            int *a = cin_array(n); /// Input
+            buble_sort5(a, n); /// Bubble sorting
+            cout_array(a, n); /// Output
             break;
         }
         case 2:
         {
             int n; /// number of elements
-            cout << "Please, enter the number of elements of array" << endl;
-            cin >> n;
-            int *a = new int [n]; /// array with integers
-            cout << "Please, enter elements of array" << endl;
-            for (int i = 0; i < n; i++)
-                cin >> a[i];
-
-            selection_sort5(a, n);
-            for (int i = 0; i < n; i++)
-                cout << a[i] << " ";
+            int *a = cin_array(n); /// Input
+            selection_sort5(a, n); /// Selection sorting
+            cout_array(a, n); /// Output
             break;
         }
         case 3:
         {
             int n; /// number of elements
-            cout << "Please, enter the number of line in square matrix" << endl;
-            cin >> n;
-            int** a = new int*[n]; /// array with integers
-            for(int i = 0; i < n; i++)
-                a[i] = new int[n];
-            cout << "Please, enter elements of array" << endl;
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++)
-                    cin >> a[i][j];
-            int sum = sum_line5(a, n, 0); /// value of sum of first line
-            /// Processing and check (line)
-            for (int i = 1; i < n; i++)
-                if (sum_line5(a, n, i) != sum)
-                {
-                    cout << "This matrix is not magic square" << endl;
-                    return 0;
-                }
-            /// Processing and check (column)
-            for (int i = 0; i < n; i++)
-                if (sum_column5(a, n, i) != sum)
-                {
-                    cout << "This matrix is not a magic square" << endl;
-                    return 0;
-                }
-            cout << "This matrix is a magic square" << endl;
+            int **a = cin_square_matrix(n); /// Input
+            if (check_magic_square(a, n))
+                cout << "This matrix is magic square" << endl;
+            else
+                cout << "This matrix is not magic square" << endl;
             break;
         }
         case 4:
         {
             int n; /// number of elements
-            cout << "Please, enter the number of elements of array" << endl;
-            cin >> n;
-            int *a = new int [n]; /// array with integers
-            cout << "Please, enter elements of array" << endl;
-            for (int i = 0; i < n; i++)
-                cin >> a[i];
-
-            insert_sort5(a, n);
-            for (int i = 0; i < n; i++)
-                cout << a[i] << " ";
+            int *a = cin_array(n); /// Input
+            insert_sort5(a, n); /// Insert sorting
+            cout_array(a, n); /// Output
             break;
         }
         case 5:
         {
             int nA; /// number of elements of array A
-            cout << "Please, enter the number of elements of array A" << endl;
-            cin >> nA;
-            int *A = new int [nA]; /// array A with integers
-            cout << "Please, enter elements of array A" << endl;
-            for (int i = 0; i < nA; i++)
-                cin >> A[i];
+            cout << "Array A:" << endl;
+            int *A = cin_array(nA); /// Input A
 
             int nB; /// number of elements of array B
-            cout << "Please, enter the number of elements of array B" << endl;
-            cin >> nB;
-            int *B = new int [nB]; /// array B with integers
-            cout << "Please, enter elements of array B" << endl;
-            for (int i = 0; i < nB; i++)
-                cin >> B[i];
+            cout << "Array B:" << endl;
+            int *B = cin_array(nB); /// Input B
 
             /// Sorting
             insert_sort5(A, nA);
             insert_sort5(B, nB);
 
+            /// Output 1
             cout << "Sorted array A: ";
-            for (int i = 0; i < nA; i++)
-                cout << A[i] << " ";
-            cout << endl;
+            cout_array(A, nA);
 
             cout << "Sorted array B: ";
-            for (int i = 0; i < nB; i++)
-                cout << B[i] << " ";
-            cout << endl;
+            cout_array(B, nB);
 
+            /// Processing and output result
             int *ptr = check_subset5(A, nA, B, nB);
             if (ptr != 0)
                 cout << ptr << " - pointer of element " << *ptr << endl;
-            if (ptr == 0)
+            else
                 cout << ptr << endl;
             break;
         }
