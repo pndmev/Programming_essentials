a66ada2883d7609fed87c2595ff921e64fc1f176
pndmev <32229048+pndmev@users.noreply.github.com>
2017-10-29 00:24:08 +0300

    New main3

--- N_3_PonyavinD.cpp
+++ N_3_PonyavinD.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+bool is_leap_year (int year)
+{
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+}
+
 /// Sum of dividers of number
 int decomposition (int x)
 {
@@ -117,39 +122,30 @@ bool function_5 (int day, int month, int year)
     }
     switch (month)
     {
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        {
+            if (day > 31)
             {
-                if (day > 31)
-                {
-                    return 0;
-                }
-                break;
+                return 0;
             }
-        case 4:
-        case 6:
-        case 9:
-        case 11:
+            break;
+        }
+        case 4: case 6: case 9: case 11:
+        {
+            if (day > 30)
             {
-                if (day > 30)
-                {
-                    return 0;
-                }
-                break;
+                return 0;
             }
+            break;
+        }
         case 2:
+        {
+            if ((day > 28 && !is_leap_year(year)) || (day > 29 && is_leap_year(year)))
             {
-                if ((day > 28 && (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0))) || (day > 29 && year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)))
-                {
-                    return 0;
-                }
-                break;
+                return 0;
             }
+            break;
+        }
         default:
         {
             return 0;
@@ -159,169 +155,158 @@ bool function_5 (int day, int month, int year)
 }
 int main3()
 {
-    int number = 0; /// number of task
-    cout << "Please, enter the number of task ( 1, 2, 3, 4, 5 )" << endl;
     cout << "1. N : number of various digits?" << endl;
     cout << "2. N : all automorphic numbers < N" << endl;
     cout << "3. N : all nonreducible fractions, denominator < N" << endl;
     cout << "4. N : all perfect numbers < N" << endl;
     cout << "5. Input: date of birth, current date. Output: number of lived days, 1(number of lived days), 2(number of lived days), 4 (number of lived days)" << endl;
-    /// Input
-    while (number == 0)
+
+    while (do_you_want_to_continue())
     {
-        cin >> number;
-        if (number < 1 || number > 5)
-        {
-            cout << "Incorrect number" << endl;
-            number = 0;
-        }
-    }
+        int number = 0; /// number of task
+        cout << "Please, enter the number of task ( 1, 2, 3, 4, 5 )" << endl;
 
-    switch (number)
-    {
-        case 1:
+        /// Input
+        while (number == 0)
         {
-            int N = 0; /// input date
-            /// Input
-            while (N == 0)
+            cin >> number;
+            if (number < 1 || number > 5)
             {
-                cout << "N = ";
-                cin >> N;
-                if (N < 1)
-                    N = 0;
+                cout << "Incorrect number" << endl;
+                number = 0;
             }
-            /// Processing and output
-            cout << function_1(N) << endl;
-            break;
         }
-        case 2:
-        {
-            int N = 0; /// input date
-            /// Input
-            while (N == 0)
-            {
-                cout << "N = ";
-                cin >> N;
-                if (N < 1 || N > 46340)
-                    N = 0;
-            }
-            /// Processing and output
-            function_2(N);
-            break;
-        }
-        case 3:
-        {
-            int N = 0; /// input date
-            /// Input
-            while (N == 0)
-            {
-                cout << "N = ";
-                cin >> N;
-                if (N < 1)
-                    N = 0;
-            }
-            /// Processing and output
-            function_3(N);
-            break;
-        }
-        case 4:
-        {
-            int N = 0; /// input date
-            /// Input
-            while (N == 0)
-            {
-                cout << "N = ";
-                cin >> N;
-                if (N < 1 || N > 1000000)
-                    N = 0;
-            }
-            /// Processing and output
-            function_4(N);
-            break;
-        }
-        case 5:
-        {
-            int d1; /// day of birth
-            int d2; /// current day
-            int m1; /// month of birth
-            int m2; /// current month
-            int y1; /// year of birth
-            int y2; /// current year
 
-            int month1[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; /// for not leap year
-            int month2[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; /// for leap year
-
-            int i; /// counter
-            int subproduct1 = 0; /// number of days which have passed in year of birth
-            int subproduct2 = 0; /// number of days which have passed in current year
-            int result = 0; /// number of lived days
-            /// Input and check
-            cout << "Please, enter the date of birth" << endl;
-            cin >> d1 >> m1 >> y1;
-
-            while (!function_5(d1, m1, y1))
+        switch (number)
+        {
+            case 1:
             {
-                cout << "Incorrect date" << endl;
+                int N = 0; /// input date
+                /// Input
+                while (N == 0)
+                {
+                    cout << "N = ";
+                    cin >> N;
+                    if (N < 1)
+                        N = 0;
+                }
+                /// Processing and output
+                cout << function_1(N) << endl;
+                break;
+            }
+            case 2:
+            {
+                int N = 0; /// input date
+                /// Input
+                while (N == 0)
+                {
+                    cout << "N = ";
+                    cin >> N;
+                    if (N < 1 || N > int(sqrt(INT_MAX)))
+                        N = 0;
+                }
+                /// Processing and output
+                function_2(N);
+                break;
+            }
+            case 3:
+            {
+                int N = 0; /// input date
+                /// Input
+                while (N == 0)
+                {
+                    cout << "N = ";
+                    cin >> N;
+                    if (N < 1)
+                        N = 0;
+                }
+                /// Processing and output
+                function_3(N);
+                break;
+            }
+            case 4:
+            {
+                int N = 0; /// input date
+                /// Input
+                while (N == 0)
+                {
+                    cout << "N = ";
+                    cin >> N;
+                    if (N < 1 || N > 1000000)
+                        N = 0;
+                }
+                /// Processing and output
+                function_4(N);
+                break;
+            }
+            case 5:
+            {
+                int d1; /// day of birth
+                int d2; /// current day
+                int m1; /// month of birth
+                int m2; /// current month
+                int y1; /// year of birth
+                int y2; /// current year
+
+                const int QuantityOfDaysInYear = 365;
+                const int month[2][12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; /// for not leap and leap years
+
+                int i; /// counter
+                int subproduct1 = 0; /// number of days which have passed in year of birth
+                int subproduct2 = 0; /// number of days which have passed in current year
+                int result = 0; /// number of lived days
+                /// Input and check
+                cout << "Please, enter the date of birth" << endl;
                 cin >> d1 >> m1 >> y1;
-            }
 
-            cout << "Please, enter the current date" << endl;
-            cin >> d2 >> m2 >> y2;
+                while (!function_5(d1, m1, y1))
+                {
+                    cout << "Incorrect date" << endl;
+                    cin >> d1 >> m1 >> y1;
+                }
 
-            while (!function_5(d2, m2, y2) || (y2 < y1) || (y2 == y1 && m2 < m1) || (y2 == y1 && m2 == m1 && d2 < d1))
-            {
-                cout << "Incorrect date" << endl;
+                cout << "Please, enter the current date" << endl;
                 cin >> d2 >> m2 >> y2;
-            }
-            /// The number of days from the begin of year of birth
-            if (y1 % 4 != 0 || (y1 % 100 == 0 && y1 % 400 != 0))
+
+                while (!function_5(d2, m2, y2) || (y2 < y1) || (y2 == y1 && m2 < m1) || (y2 == y1 && m2 == m1 && d2 < d1))
+                {
+                    cout << "Incorrect date" << endl;
+                    cin >> d2 >> m2 >> y2;
+                }
+                bool isLeapYear1 = is_leap_year(y1);
+                bool isLeapYear2 = is_leap_year(y2);
+                /// The number of days from the begin of year of birth
                 for (i = 0; i < m1-1; i++)
-                    subproduct1+= month1[i];
-            else
-                for (i = 0; i < m1-1; i++)
-                    subproduct1 += month2[i];
-            subproduct1 += d1;
-            /// The number of days from the begin of current year
-            if (y2 % 4 != 0 || (y1 % 100 == 0 && y1 % 400 != 0))
+                    subproduct1+= month[!isLeapYear1][i];
+                subproduct1 += d1;
+                /// The number of days from the begin of current year
                 for (i = 0; i < m2-1; i++)
-                    subproduct2 += month1[i];
-            else
-                for (i = 0; i < m2-1; i++)
-                    subproduct2 += month2[i];
-            subproduct2 += d2;
-            /// Sum of days in intermediate years
-            for (i = y1 + 1; i < y2; i++)
-            {
-                if (i % 4 != 0)
-                    result += 365;
-                else result += 366;
-            }
-            /// Processing
-            if (y1 == y2)
-                result += subproduct2 - subproduct1;
-            else
-            {
-                if (y1 % 4 != 0 || (y1 % 100 == 0 && y1 % 400 != 0))
-                    subproduct1 = 365 - subproduct1;
+                    subproduct2 += month[!isLeapYear2][i];
+                subproduct2 += d2;
+                /// Sum of days in intermediate years
+                for (i = y1 + 1; i < y2; i++)
+                    result += QuantityOfDaysInYear + is_leap_year(i);
+                /// Processing
+                if (y1 == y2)
+                    result += subproduct2 - subproduct1;
                 else
-                    subproduct1 = 366 - subproduct1;
-                result += subproduct1 + subproduct2;
-            }
-            /// Output
-            cout << result << endl;
-            /// Processing and output
-            cout << "1) " << function_1(result) << endl;
-            cout << "2) ";
-            if (result <= 46340)
-                function_2(result);
-            else
-                cout << "Too large number" << endl;
-            cout << "4) ";
-            if (result < 1000000)
+                {
+                    subproduct1 = QuantityOfDaysInYear + isLeapYear1 - subproduct1;
+                    result += subproduct1 + subproduct2;
+                }
+                /// Output
+                cout << result << endl;
+                /// Processing and output
+                cout << "1) " << function_1(result) << endl;
+                cout << "2) ";
+                if (result <= int(sqrt(INT_MAX)))
+                    function_2(result);
+                else
+                    cout << "Too large number" << endl;
+                cout << "4) ";
                 function_4(result);
-            else
-                cout << "Too large number" << endl;
-            break;
+                break;
+            }
         }
     }
     return 0;
